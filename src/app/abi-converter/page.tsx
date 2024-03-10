c5e6362850d78c11ba3230ff2d5a4fbfae14da5c
kinsyudev <miguel@solarlabs.gg>
2024-03-10 13:13:57 +0100

    added shadcn components

--- src/app/abi-converter/page.tsx
+++ src/app/abi-converter/page.tsx
@@ -1,13 +1,5 @@
 import Link from "next/link";
 
-const Routes = [
-  {
-    name: "",
-    description: "",
-    path: "",
-  },
-];
-
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -16,7 +8,6 @@ export default function HomePage() {
           kTools
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
             href="https://create.t3.gg/en/usage/first-steps"
